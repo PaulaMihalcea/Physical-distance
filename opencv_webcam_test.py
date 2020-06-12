b1128f92e2b47f5f7cfa504020d79c716d003b4b
zelethil <mihalceapaula@gmail.com>
2020-06-12 19:08:10 +0200

    First commit

---
+++ opencv_webcam_test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)  # 0 or -1 For default camera, 1 for next one and so on; passing a string containing a path/filename opens an external video file
+
+save_to = 'output.avi'
+fourcc = cv2.VideoWriter_fourcc(*'XVID') # Defines the codec and creates a VideoWriter object
+out = cv2.VideoWriter(save_to, fourcc, 20.0, (640,480))
+
+frame_number = 0  # Frame counter
+position = (5, int(cap.get(4) - 10))  # Text position
+
+while True:
+
+    if not cap.isOpened():  # Check that the capture has been initialized
+        cap.open()
+
+    ret, frame = cap.read()  # Frame by frame capture; returns a boolean: True if the frame has been read correctly, False otherwise; also returns a frame
+    frame_number += 1
+    f = str(frame_number)
+
+    # Frame processing
+    cv2.putText(frame, f, position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0, 255), 3)
+
+    out.write(frame)
+
+    cv2.imshow('Webcam', frame)
+    k = cv2.waitKey(33)
+    if k == 27:
+        break
+
+cap.release()  # Release the capture when finished
+out.release()
+cv2.destroyAllWindows()
